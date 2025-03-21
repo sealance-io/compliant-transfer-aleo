f55bc34643a1dea39d128f2ffe55ba1a13f29d13
cryptograph <gribov.alesha@gmail.com>
2025-03-21 10:02:05 -0400

    improve scripts

--- scripts/deploy-devnet.ts
+++ scripts/deploy-devnet.ts
@@ -5,12 +5,13 @@ import { RediwsozfoContract } from "../artifacts/js/rediwsozfo";
 import { TqxftxoicdContract } from "../artifacts/js/tqxftxoicd";
 import { deployIfNotDeployed } from "../lib/Deploy";
 import { BaseContract } from '../contract/base-contract';
-import { mode } from "./Constants";
+import { fundedAmount } from "../lib/Constants";
 import { initializeTokenProgram } from "../lib/Token";
+import { fundWithCredits } from "../lib/Fund";
 
 const mode = ExecutionMode.SnarkExecute;
 const contract = new BaseContract({ mode });
-const [deployerAddress] = contract.getAccounts();
+const [deployerAddress, adminAddress, investigatorAddress] = contract.getAccounts();
 const deployerPrivKey = contract.getPrivateKey(deployerAddress);
 
 const tokenRegistryContract = new Token_registryContract({ mode, privateKey: deployerPrivKey });
@@ -18,13 +19,15 @@ const compliantTransferContract = new TqxftxoicdContract({ mode })
 const merkleTreeContract = new RediwsozfoContract({ mode });
 
 (async () => {
+    await fundWithCredits(deployerPrivKey, adminAddress, fundedAmount);
+
     // deploy contracts
     await deployIfNotDeployed(tokenRegistryContract);
     await deployIfNotDeployed(merkleTreeContract);
     await deployIfNotDeployed(compliantTransferContract);
 
     // register token and assign compliant transfer contract as external_authorization_party
-    await initializeTokenProgram();
+    await initializeTokenProgram(deployerPrivKey, deployerAddress, adminAddress, investigatorAddress);
 
     process.exit(0);
 })();
