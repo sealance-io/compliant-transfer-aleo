5dfa535a023dbffdc4a38b53592d69fe638c90ed
Nadav Peled <peled.nadav@gmail.com>
2025-04-08 12:07:48 +0300

    implement and test a policy that uses freeze_registry

--- lib/Token.ts
+++ lib/Token.ts
@@ -1,12 +1,9 @@
 import { Token_registryContract } from "../artifacts/js/token_registry";
-import { Tqxftxoicd_v2Contract } from "../artifacts/js/tqxftxoicd_v2";
 import { ZERO_ADDRESS, mode, tokenId, tokenName, tokenSymbol } from "./Constants";
 import { stringToBigInt } from "./Conversion";
 
-export async function initializeTokenProgram(deployerPrivKey: any, deployerAddress: string, adminAddress: string, investigatorAddress: string) {
-
+export async function initializeTokenProgram(deployerPrivKey: any, deployerAddress: string, adminAddress: string, policyAddress: string) {
     const tokenRegistryContract = new Token_registryContract({ mode, privateKey: deployerPrivKey });
-    const compliantTransferContract = new Tqxftxoicd_v2Contract({ mode, privateKey: deployerPrivKey });
 
     // register token and assign compliant transfer contract as external_authorization_party
     const tokenMetadata = await tokenRegistryContract.registered_tokens(
@@ -31,41 +28,42 @@ export async function initializeTokenProgram(deployerPrivKey: any, deployerAddre
             6, // decimals
             1000_000000000000n, // max supply
             true,
-            compliantTransferContract.address()
+            policyAddress
         );
         await tx.wait();
         if (deployerAddress !== adminAddress) {
             const tx = await tokenRegistryContract.update_token_management(
                 tokenId,
                 adminAddress,
-                compliantTransferContract.address()
+                policyAddress
             )
             await tx.wait();
         }
-    } else if (tokenMetadata.external_authorization_party !== compliantTransferContract.address()) {
+    } else if (tokenMetadata.external_authorization_party !== policyAddress) {
+        // if the admin is not the deployer and the admin is already the admin this call will not work
         const tx = await tokenRegistryContract.update_token_management(
             tokenId,
             adminAddress,
-            compliantTransferContract.address()
+            policyAddress
         )
         await tx.wait();
     }
 
-    const adminRole = await compliantTransferContract.roles(1, ZERO_ADDRESS);
-    if (adminRole === ZERO_ADDRESS) {
-        const tx = await compliantTransferContract.update_admin_address(deployerAddress);
-        await tx.wait();
-    }
+    // const adminRole = await compliantTransferContract.roles(1, ZERO_ADDRESS);
+    // if (adminRole === ZERO_ADDRESS) {
+    //     const tx = await compliantTransferContract.update_admin_address(deployerAddress);
+    //     await tx.wait();
+    // }
 
-    const investigatorRole = await compliantTransferContract.roles(2, ZERO_ADDRESS);
-    if (investigatorRole === ZERO_ADDRESS) {
-        const tx = await compliantTransferContract.update_investigator_address(investigatorAddress);
-        await tx.wait();
-    }
+    // const investigatorRole = await compliantTransferContract.roles(2, ZERO_ADDRESS);
+    // if (investigatorRole === ZERO_ADDRESS) {
+    //     const tx = await compliantTransferContract.update_investigator_address(investigatorAddress);
+    //     await tx.wait();
+    // }
 
-    if (adminRole === ZERO_ADDRESS) {
-        const tx = await compliantTransferContract.update_admin_address(adminAddress);
-        await tx.wait();
-    }
+    // if (adminRole === ZERO_ADDRESS) {
+    //     const tx = await compliantTransferContract.update_admin_address(adminAddress);
+    //     await tx.wait();
+    // }
 
 }
