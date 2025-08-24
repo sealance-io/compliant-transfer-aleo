dd81b16ecab3aebf5fa22c8e54914b7ce880c768
Or Noyman <17881579+fullkomnun@users.noreply.github.com>
2025-08-24 16:06:39 +0300

    double checking that a program has indeed been deployed to prevent issues

--- lib/Deploy.ts
+++ lib/Deploy.ts
@@ -1,9 +1,14 @@
 import { BaseContract } from "../contract/base-contract";
 
 export async function deployIfNotDeployed(contract: BaseContract) {
-  const isDeployed = await contract.isDeployed();
+  let isDeployed = await contract.isDeployed();
   if (!isDeployed) {
     const tx = await contract.deploy();
     await tx.wait();
+    // verifying that program has been deployed
+    isDeployed = await contract.isDeployed();
+    if (!isDeployed) {
+      throw new Error(`deployment has failed`);
+    }
   }
 }
