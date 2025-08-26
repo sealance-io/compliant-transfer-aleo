2fe50d800a82246c926f1b360641e66cad51c639
Nadav Peled <peled.nadav@gmail.com>
2025-08-26 19:42:39 +0300

    Deploy the report token in the deployment scripts

--- lib/Initalize.ts
+++ lib/Initalize.ts
@@ -1,4 +1,4 @@
-import { CURRENT_FREEZE_LIST_ROOT_INDEX } from "./Constants";
+import { BLOCK_HEIGHT_WINDOW, CURRENT_FREEZE_LIST_ROOT_INDEX } from "./Constants";
 
 export async function isProgramInitialized(program: any) {
   try {
@@ -6,5 +6,27 @@ export async function isProgramInitialized(program: any) {
     return true;
   } catch {
     return false;
+  }
+}
+
+export async function initializeFreezeListAndTokenDetails(
+  contract: any,
+  name: bigint,
+  symbol: bigint,
+  decimals: number,
+  maxSupply: bigint,
+) {
+  const isInitialized = await isProgramInitialized(contract);
+  if (!isInitialized) {
+    const tx = await contract.initialize(name, symbol, decimals, maxSupply, BLOCK_HEIGHT_WINDOW);
+    await tx.wait();
+  }
+}
+
+export async function initializeFreezeList(contract: any) {
+  const isInitialized = await isProgramInitialized(contract);
+  if (!isInitialized) {
+    const tx = await contract.initialize(BLOCK_HEIGHT_WINDOW);
+    await tx.wait();
   }
 }
