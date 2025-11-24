a3624a07e584bca2306386fcbc0e48c0b3c53cd0
Or Noyman <17881579+fullkomnun@users.noreply.github.com>
2025-11-24 18:58:30 +0200

    remove endpoint cheeck from upgrade util

--- lib/Upgrade.ts
+++ lib/Upgrade.ts
@@ -7,12 +7,11 @@ const execAsync = promisify(exec);
 export async function upgradeProgram(programName: string, privateKey: string) {
   const networkName = networkConfig.defaultNetwork;
   const endpoint = networkConfig.networks[networkName].endpoint;
-  const isDevnet = endpoint.includes("localhost") || endpoint.includes("host.docker.internal");
   console.log(
-    `cd artifacts/leo/${programName} && leo upgrade --broadcast ${isDevnet ? "--devnet" : ""} --private-key ${privateKey} --yes --endpoint ${endpoint} --network ${networkName} --blocks-to-check 20 --max-wait 30`,
+    `cd artifacts/leo/${programName} && leo upgrade --broadcast --private-key ${privateKey} --yes --endpoint ${endpoint} --network ${networkName} --blocks-to-check 20 --max-wait 30`,
   );
   const { stdout } = await execAsync(
-    `cd artifacts/leo/${programName} && leo upgrade --broadcast ${isDevnet ? "--devnet" : ""} --private-key ${privateKey} --yes --endpoint ${endpoint} --network ${networkName} --blocks-to-check 20 --max-wait 30`,
+    `cd artifacts/leo/${programName} && leo upgrade --broadcast --private-key ${privateKey} --yes --endpoint ${endpoint} --network ${networkName} --blocks-to-check 20 --max-wait 30`,
   );
   if (stdout.includes("Upgrade confirmed!")) {
     return true;
