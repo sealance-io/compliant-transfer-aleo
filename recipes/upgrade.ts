49e98b9ef54e6afa27b2a33ee72f4e156390acd0
Nadav Peled <peled.nadav@gmail.com>
2026-05-27 15:13:01 +0300

    migrate scripts

---
+++ recipes/upgrade.ts
@@ -0,0 +1,21 @@
+import type { LionDenRuntimeEnvironment } from "@lionden/core";
+import { upgradeProgram } from "../lib/Upgrade.js";
+
+export default async function (lre: LionDenRuntimeEnvironment) {
+  const programFlagIndex = process.argv.indexOf("--program");
+
+  if (programFlagIndex === -1 || !process.argv[programFlagIndex + 1]) {
+    console.error("Expected --program argument! Usage: npx tsx scripts/upgrade.ts --program <programName> [testnet]");
+    process.exit(1);
+  }
+
+  const programName = process.argv[programFlagIndex + 1];
+
+  const isUpgradeSuccessful = await upgradeProgram(lre, programName, {
+    generateTypechain: true,
+  });
+
+  if (!isUpgradeSuccessful) {
+    console.error(`Upgrading ${programName} failed`);
+  }
+}
