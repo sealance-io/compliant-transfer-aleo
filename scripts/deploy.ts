925b35d3eb07aeb6a1bec1a8c67c12d8ed2047b9
Nadav Peled <peled.nadav@gmail.com>
2026-06-07 19:02:19 +0300

    fix scripts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,6 +1,7 @@
 import type { LionDenRuntimeEnvironment } from "@lionden/core";
 
 export default async function (lre: LionDenRuntimeEnvironment) {
+  console.log(lre.network.activeConnection.name);
   await lre.tasks.run("compile");
-  await lre.tasks.run("recipe", { file: "recipes/setup.ts" });
+  await lre.tasks.run("recipe", { file: "recipes/setup.ts", network: lre.network.activeConnection.name });
 }
