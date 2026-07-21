31e27b4bd2baff5ea0a5a25b84f57507eecd1ce6
Nadav Peled <peled.nadav@gmail.com>
2026-07-22 01:54:04 +0300

    update deploy call

--- scripts/deploy.ts
+++
@@ -1,7 +0,0 @@
-import type { LionDenRuntimeEnvironment } from "@lionden/core";
-
-export default async function (lre: LionDenRuntimeEnvironment) {
-  console.log(lre.network.activeConnection.name);
-  await lre.tasks.run("compile");
-  await lre.tasks.run("recipe", { file: "recipes/setup.ts", network: lre.network.activeConnection.name });
-}
